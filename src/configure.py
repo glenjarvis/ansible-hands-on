89804af6fff5c043fa212cdc1b68e2acd33f0f9d
Abe Young <plazith@gmail.com>
2014-08-14 16:19:04 +0000

    fixed length issue

--- src/configure.py
+++ src/configure.py
@@ -77,7 +77,8 @@ def configure_config():
         print "Nope. This file cannot be found: {0}".format(pem_file_path)
         sys.exit(1)
 
-    print "\nWhat user to use to ssh to the remote system?\nIf no user is specified, ec2-user will be chosen as user."
+    print "\nWhat user to use to ssh to the remote system?"
+    print "\nIf no user is specified, ec2-user will be chosen as user."
     user = raw_input('--> ') or 'ec2-user'
     user = user.strip()
 
