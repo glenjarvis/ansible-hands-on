21a93afd9e5556a5be5a97d113995834fdb3e719
Glen Jarvis <glen@glenjarvis.com>
2019-12-02 12:23:13 -0800

    [Fixes #78] Fix stale Python and Ansible
    Bare minimum to run on Python3 and current version of Ansible.

--- ansible_tutorial/cli.py
+++ ansible_tutorial/cli.py
@@ -8,9 +8,6 @@ import click
 @click.command()
 def main(args=None):
     """Console script for ansible_tutorial."""
-    click.echo("Replace this message by putting your code into "
-               "ansible_tutorial.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
 
--- src/configure.py
+++ src/configure.py
@@ -9,7 +9,7 @@ gotcha (permissions on the downloaded key file) by setting the
 permissions to be Read/Write for only the owner.
 """
 
-import ConfigParser
+import configparser
 import os
 import stat
 import sys
@@ -43,13 +43,13 @@ def write_ansible_cfg_file(pem_file_path, user):
     """Given a validated pem_file_path, write the ansible configuration file"""
 
     fix_pem_permissons(pem_file_path)
-    config = ConfigParser.RawConfigParser()
+    config = configparser.ConfigParser()
     config.add_section('defaults')
-    config.set('defaults', 'hostfile', ANSIBLE_HOSTS_FILENAME)
-    config.set('defaults', 'private_key_file', pem_file_path)
-    config.set('defaults', 'remote_user', user)
+    config['DEFAULT'] = {'inventory': ANSIBLE_HOSTS_FILENAME,
+                         'private_key_file': pem_file_path,
+                         'remote_user': user}
 
-    with open(ANSIBLE_CONFIG_FILENAME, 'wb') as config_file:
+    with open(ANSIBLE_CONFIG_FILENAME, 'w') as config_file:
         config.write(config_file)
 
 
@@ -57,29 +57,29 @@ def configure_config():
 
     """Assuming no ansible.cfg exists, ask questions and create a new one"""
 
-    print """
+    print("""
     No configuration file found. Let me ask questions so that we can configure.
-    """
+    """)
 
-    print "What is the path to your .pem key file for the virtual machine?"
-    print "Be sure to include the filename in the path."
-    pem_file_path = raw_input('--> ')
+    print("What is the path to your .pem key file for the virtual machine?")
+    print("Be sure to include the filename in the path.")
+    pem_file_path = input('--> ')
     pem_file_path = pem_file_path.strip()
     pem_file_path = os.path.expanduser(pem_file_path)  # expand ~user
 
     if os.path.isdir(pem_file_path):
-        print "We mean the path to (including the file name)."
-        print "You have entered a directory, but no file name: {0}".format(
-            pem_file_path)
+        print("We mean the path to (including the file name).")
+        print("You have entered a directory, but no file name: {0}".format(
+            pem_file_path))
         sys.exit(1)
 
     if not os.path.exists(pem_file_path):
-        print "Nope. This file cannot be found: {0}".format(pem_file_path)
+        print("Nope. This file cannot be found: {0}".format(pem_file_path))
         sys.exit(1)
 
-    print "\nWhat is the ssh remote system username [ec2-user]?"
-    print "Press ENTER to accept 'ec2-user' as default user."
-    user = raw_input('--> ') or 'ec2-user'
+    print("\nWhat is the ssh remote system username [ec2-user]?")
+    print("Press ENTER to accept 'ec2-user' as default user.")
+    user = input('--> ') or 'ec2-user'
     user = user.strip()
 
     write_ansible_cfg_file(pem_file_path, user)
@@ -89,26 +89,35 @@ def get_configured_hosts():
 
     """Read the ansible.cfg file and parse hostfile pathname"""
 
-    config = ConfigParser.SafeConfigParser()
+    config = configparser.ConfigParser()
     config.read(ANSIBLE_CONFIG_FILENAME)
 
     hostfile = config.get('defaults', 'hostfile')
     if hostfile is None:
-        print "We can't read the hostfile settings from {0}".format(
-            ANSIBLE_CONFIG_FILENAME)
+        print("We can't read the hostfile settings from {0}".format(
+            ANSIBLE_CONFIG_FILENAME))
         sys.exit(2)
 
     return hostfile
 
+#>>> config.sections()
+#['bitbucket.org', 'topsecret.server.com']
+#>>> 'bitbucket.org' in config
+#True
+#>>> 'bytebong.com' in config
+#False
+#>>> config['bitbucket.org']['User']
+#'hg'
+#>>> config['
 
 def configure_hosts(hostfile):
 
     """Assuming no hostfile file exists, ask questions and create a new one"""
 
-    print "Configuring `ansible_hosts` file {0}...\n".format(hostfile)
+    print("Configuring `ansible_hosts` file {0}...\n".format(hostfile))
 
-    print "What is the IP address of the virtual machine?"
-    machine_address = raw_input('--> ')
+    print("What is the IP address of the virtual machine?")
+    machine_address = input('--> ')
     machine_address = machine_address.strip()
 
     with open(hostfile, 'w') as ansible_hosts_file:
@@ -119,11 +128,11 @@ def configure_hosts(hostfile):
 def check_and_configure():
     """Check/configure `ansible.cfg` and `ansible_hosts`"""
 
-    print "\nThis script creates configuration files for using Ansible to "
-    print "configure a newly-created virtual machine."
-    print "It has been tested by the author on an AWS free tier VM."
-    print "This has the best chance of working on an AWS free tier VM, or "
-    print "failing that, on a VM with a recent version of CentOS."
+    print("\nThis script creates configuration files for using Ansible to ")
+    print("configure a newly-created virtual machine.")
+    print("It has been tested by the author on an AWS free tier VM.")
+    print("This has the best chance of working on an AWS free tier VM, or ")
+    print("failing that, on a VM with a recent version of CentOS.")
 
     if not os.path.exists(ANSIBLE_CONFIG_FILEPATH):
         configure_config()
@@ -135,7 +144,7 @@ def check_and_configure():
     if not os.path.exists(hostfile):
         configure_hosts(hostfile)
 
-    print "\nConfiguration is complete."
+    print("\nConfiguration is complete.")
 
 
 if __name__ == '__main__':
